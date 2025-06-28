f9a3c4a83d8a08b75301f0174d20785db1c75bd2
QziuL <lf.quinholi2@gmail.com>
2025-06-28 20:49:21 -0300

    refactor: routes with middleware for admin

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,16 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return json_encode('Welcome to LumenIF API.');
 });
 
-Route::get('/user/{id}', [UserController::class, 'getOne'])->middleware('auth:sanctum');
+Route::middleware(['auth:sanctum', 'role:ADMIN'])->prefix('admin')->group(function () {
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/users/{id}', [UserController::class, 'show']);
+});
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
