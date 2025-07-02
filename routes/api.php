ad124260f1ce42d4bb21ab96dddb5f9461b686c1
QziuL <lf.quinholi2@gmail.com>
2025-07-02 01:34:27 -0300

    feat: created routes for user management and roles

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,10 +9,22 @@ Route::get('/', function () {
     return json_encode('Welcome to LumenIF API.');
 });
 
+// Rotas ADMIN
 Route::middleware(['auth:sanctum', 'role:ADMIN'])->prefix('admin')->group(function () {
-    Route::get('/users', [UserController::class, 'index']);
-    Route::get('/users/{id}', [UserController::class, 'show']);
+    // Rotas para gerenciamento de usuários
+    Route::prefix('users')->group(function () {
+        Route::get('', [UserController::class, 'index']);
+        Route::get('/{id}', [UserController::class, 'show']);
+        Route::post('', [UserController::class, 'store']);
+        Route::put('/{id}', [UserController::class, 'update']);
+        Route::delete('/{id}', [UserController::class, 'destroy']);
+    });
+    // Rota para gerenciamento de roles
+    Route::prefix('roles')->group(function () {
+        Route::get('', [RoleController::class, 'index']);
+    });
 });
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
