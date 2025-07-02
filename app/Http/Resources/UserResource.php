9f0f07229d609a895876a6439f386c05e4fdbcd7
QziuL <lf.quinholi2@gmail.com>
2025-07-02 01:30:45 -0300

    feat: created resource to transform request values of user

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray($request): array
+    {
+        return [
+            'public_id' => $this->public_id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'roles' => $this->roles->map(function ($role) {
+                return [
+                    'id' => $role->id,
+                    'name' => $role->name,
+                ];
+            })->values(),
+        ];
+    }
+}
